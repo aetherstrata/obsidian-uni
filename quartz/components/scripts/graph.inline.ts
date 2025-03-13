f0ded29310c3f11b5f3f8768decb81a3eb27c420
Jacky Zhao <j.zhao2k19@gmail.com>
2025-03-13 12:29:30 -0700

    fix(graph): avoid keeping stroke in gfx

--- quartz/components/scripts/graph.inline.ts
+++ quartz/components/scripts/graph.inline.ts
@@ -397,7 +397,6 @@ async function renderGraph(graph: HTMLElement, fullSlug: FullSlug) {
     })
       .circle(0, 0, nodeRadius(n))
       .fill({ color: isTagNode ? computedStyleMap["--light"] : color(n) })
-      .stroke({ width: isTagNode ? 2 : 0, color: color(n) })
       .on("pointerover", (e) => {
         updateHoverInfo(e.target.label)
         oldLabelOpacity = label.alpha
@@ -412,6 +411,10 @@ async function renderGraph(graph: HTMLElement, fullSlug: FullSlug) {
           renderPixiFromD3()
         }
       })
+
+    if (isTagNode) {
+      gfx.stroke({ width: 2, color: computedStyleMap["--tertiary"] })
+    }
 
     nodesContainer.addChild(gfx)
     labelsContainer.addChild(label)
